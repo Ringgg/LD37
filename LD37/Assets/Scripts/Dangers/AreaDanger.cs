f40a111c41f87cea69227a38090251d5324da453
MarcinJozwik <marty.jozwik@gmail.com>
2016-12-10 17:16:33 +0100

    Danger planes

---
+++ LD37/Assets/Scripts/Dangers/AreaDanger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class AreaDanger : Danger
+{
+    public float damage = 20f;
+    Hero tmp;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        tmp = col.GetComponent<Hero>();
+        if (tmp == null) return;
+        heroesIn.Add(tmp);
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        tmp = col.GetComponent<Hero>();
+        if (tmp == null) return;
+        heroesIn.Remove(tmp);
+    }
+
+    protected void GiveHeroesDamage()
+    {
+        foreach (var hero in heroesIn)
+        {
+            if (hero == null) continue;
+            hero.TakeDamage(damage);
+        }
+    }
+}
---
+++ LD37/Assets/Scripts/Dangers/ExplosionPlane.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ExplosionPlane : AreaDanger
+{
+    public ExplosionPlane secondPlane;
+
+    public override bool IsInDanger(Hero hero)
+    {
+        return heroesIn.Contains(hero);
+    }
+
+    public override Vector3 GetEscapePosition(Hero hero)
+    {
+        Vector3 dest;
+        if (secondPlane.isActiveAndEnabled)
+        {
+            dest = -(Vector3.zero - hero.transform.position);
+        }
+        else
+        {
+            dest = -(transform.position - hero.transform.position);
+        }
+        dest.y = hero.transform.position.y;
+        return hero.transform.position + dest.normalized;
+    }
+
+    void OnDestroy()
+    {
+        GiveHeroesDamage();
+    }
+}
