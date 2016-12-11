c1c26767d04b4cbeb1fe92c4f6b3ee5c667a2f11
Kuba <ringlan@gmail.com>
2016-12-11 15:09:30 +0100

    added ui icons. added start button. added boss hp bar. created ui and spawner prefabs

---
+++ LD37/Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainUI : MonoBehaviour
+{
+    public Button startButton;
+    public Image bossHP;
+
+    public void Awake()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void StartPressed()
+    {
+        startButton.gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OnGUI()
+    {
+        bossHP.fillAmount = (float) Boss.instance.hp / Boss.instance.maxHp;
+    }
+}
