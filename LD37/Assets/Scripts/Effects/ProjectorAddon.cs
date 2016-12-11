52283a9f9ea5eefa10c6bb6bf6b709654a256279
Kuba <ringlan@gmail.com>
2016-12-11 11:55:35 +0100

    -new explosion warning effects

---
+++ LD37/Assets/Scripts/Effects/ProjectorAddon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectorAddon : MonoBehaviour
+{
+    Projector p;
+
+	void Start ()
+    {
+        p = GetComponent<Projector>();
+	}
+	
+	void Update ()
+    {
+        p.aspectRatio = transform.parent.localScale.x / transform.parent.localScale.z;
+        p.orthographicSize = transform.parent.localScale.z / 2;
+
+        transform.Rotate(0, Time.deltaTime * 60, 0, Space.World);
+        transform.localPosition -= Vector3.up * Time.deltaTime;
+	}
+}
